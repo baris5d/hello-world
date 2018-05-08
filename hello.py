e74111f42962e1e8d4902957e4d4ccaf217bc1a4
Barış <baris@barisdede.com>
2018-05-08 17:01:45 +0300

    Better

--- hello.py
+++ hello.py
@@ -1,22 +1,22 @@
 from time import sleep
 import os
 
-hello = ["H","e","l","l","o",",","W","o","r","l","d"] #Aranacak Kelime
+#hello = ["H","e","l","l","o",",","W","o","r","l","d"] #Aranacak Kelime {Berat Başkan'dan Trick'ler}
+hello = "Hello, World"
+hello.split()
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",",",".","+","-","_","="," "] #Alfabe
 
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",",",".","+","-","_","="] #Alfabe
-
-result = '' # oluşan kelimeler
-hexresult = '' # oluşan kelimelerin 0-1 cinsinden değeri
-for x in hello: # her harf için ara
-    for y in alphabet: # alfabeyi gez
-        print(hexresult , end="\r") #oluşan anlık değeri ekrana bas
-        #print(result+y, end="\r")
-        if(ord(y)==ord(x)): # elde edilen değer doğruysa 
-            hexresult = hexresult + str(ord(y)%2) # hex kodunu al
-            result = result + y # oluşan değeri ekle
-            break #for y döngüsünü kır
-        print(hexresult + str(ord(y)%2), end="\r") # 0 - 1 değeri ekrana bas    
-        sleep(0.05) # birazcık yavaşla hızlı hızlı yapıyon bişi anlamıyoz
-print("I Found it!") # ehehueheuh buldu 
-sleep(1) # şşş bekle biraz
-print(result, end="\r") #Sonuç!
+result = '' 
+i = 0
+for x in hello:
+    bulundu = False
+    while(bulundu==False):
+        sleep(0.02)
+        print(result + alphabet[i] , end="\r") 
+        if(ord(alphabet[i])==ord(x)):
+            bulundu = True
+            result = result + alphabet[i]
+            i = 0 
+            break
+        i = i + 1
+print(result, end="\r") 
